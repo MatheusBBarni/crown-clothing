f9abf8cc047b25e3b2b5007ff95ec7e1cef7e14c
matheusBBarni <matheus@demacode.com.br>
2020-11-01 00:36:05 -0300

    firebase added

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+firebase.initializeApp(config);
